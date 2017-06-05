c0cc0608a3b4ace756e3cd5c71fb1cd21158390a
ganesa <ganeshaghav12@gmail.com>
2017-06-05 15:37:20 +0530

    first commit

---
+++ app/src/main/java/ganesh/com/cameraflashonoff/MainActivity.java
@@ -0,0 +1,80 @@
+package ganesh.com.cameraflashonoff;
+
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean isLightOn;
+    private Camera camera;
+    private Button button;
+    private View background;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        background = findViewById(R.id.backgroundView);
+
+        button = (Button) findViewById(R.id.buttonFlashlight);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                if (isLightOn) {
+                    turnLightOff();
+                } else {
+                    turnLightOn();
+                }
+            }
+        });
+
+        turnLightOff();
+    }
+
+    @Override
+    // release camera so other apps can use it
+    protected void onStop() {
+        super.onStop();
+        turnLightOff();
+    }
+
+    private void turnLightOff() {
+        Log.e("err", "Light off!");
+        isLightOn = false;
+
+        background.setBackgroundColor(Color.BLACK);
+
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
+    }
+
+    private void turnLightOn() {
+        Log.e("err", "Light on!");
+        isLightOn = true;
+
+        background.setBackgroundColor(Color.WHITE);
+
+        if (hasFlash()) {
+            camera = Camera.open();
+            Parameters p = camera.getParameters();
+            p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(p);
+            camera.startPreview();
+        }
+    }
+
+    private boolean hasFlash(){
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    }
+}
